7a75284f39cb56e00474d0d874e3aa101bf564fb
Martin Majlis <martin@majlis.cz>
2020-10-28 19:20:46 +0000

    Automatic data update - download - 2020-10-28 19:20:21+00:00

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,7 +7,7 @@ import requests
 
 def download(url: str, path: str, cache_lifetime=3600) -> IO:
     if not os.path.exists(path) or os.path.getmtime(path) < time.time() - cache_lifetime:
-        response = requests.get(url, allow_redirects=True)
+        response = requests.get(url, allow_redirects=True, timeout=5)
         print(f"Downloading {url} into {path}")
         with open(path, 'w') as fh:
             fh.write(response.text)
